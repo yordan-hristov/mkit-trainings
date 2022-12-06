2669c4d65ac3c4e1764e80aaf5171ad25045c0c2
Martin Bonchev <martin.bonchev@mkit.io>
2022-12-06 11:33:46 +0200

    feat: js commons index assessment functions

---
+++ quests/3-javascript-essentials/javascript-commons/index.js
@@ -0,0 +1,27 @@
+// Assessment 1
+
+function map(array, callback = () => {}) {}
+
+// Assessment 2
+
+function* fibonacci() {}
+
+// Assessment 3
+
+function potentialVotersResult(votersList) {}
+
+// Assessment 4
+
+function reduce(array, callback, initializer) {}
+
+// Assessment 5
+
+function calculateEvenAverage(array) {}
+
+module.exports = {
+  map,
+  fibonacci,
+  potentialVotersResult,
+  reduce,
+  calculateEvenAverage,
+};
