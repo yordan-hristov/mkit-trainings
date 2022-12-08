50642f7fefc88da1abb8fa36b4c0261b162c7ec7
Zhivko Uzunov <zhivko.uzunov@mkit.io>
2022-12-08 18:34:05 +0200

    feature: add first exercise

---
+++ course-lvl-up-fe-1/module-06-web-development-advanced/http/index.js
@@ -0,0 +1,17 @@
+/**
+ * Exercise 1 - Send GET Request
+ */
+
+const apiUrl = 'https://jsonplaceholder.typicode.com';
+
+function getPostData() {}
+
+/*********************************************
+ * DO NOT MODIFY THIS AREA
+ *
+ * This area is used by the automated tests.
+ ********************************************/
+
+export {
+    getPostData
+  };
