5d1308c2eac8c863065bf5154b751c722b572f53
Zhivko Uzunov <zhivko.uzunov@mkit.io>
2022-12-09 13:07:06 +0200

    feature: extend first exercise

--- course-lvl-up-fe-1/module-06-web-development-advanced/http/README.md
+++ course-lvl-up-fe-1/module-06-web-development-advanced/http/README.md
@@ -6,14 +6,14 @@ Machine communication can be described as the process of exchanging information 
 
 [Learning Materials About This Topic](https://www.notion.so/mkit/HTTP-9398181e010841529abd2ba6ba51657d)
 
-## Exercise #1 - Send GET Request
+## Exercise #1 - HTTP Methods
 
-In this Exercise you are provided with `getPostData` function and API `https://jsonplaceholder.typicode.com`. Function should receive id of type number and return data about the post with given id.
+In this Exercise, you are provided with `getData`, `postData`, `putData`, `deleteData` function declarations and API `https://jsonplaceholder.typicode.com`.
 
 Your objectives are to:
 
-- Send GET request and get data about the post with given id, API endpoint is `/posts/{id}`
-- Post ids should be between 1 and 100 and should be numbers, otherwise should throw error
+- All functions should be asynchronous 
+- Send GET request and get data about the post with given id, API endpoint is `/posts/{id}`. Use `getData` for this method. The function should receive id as argument, which should be number between 1 and 100.
 
 Example:
 
@@ -28,4 +28,48 @@ Example:
     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
 }
+```
+
+- Send POST request and make new post, API endpoint is `/posts`. Use `postData` for this method. The function should receive body as argument, which should be in the following format:
+
+```js
+// Sample input
+{
+    "userId": 1,
+    "title": "Post title",
+    "body": "Post body"
+}
+
+// Sample output
+{
+    "id": 13
+}
+```
+
+- Send PUT request and overwrite post, API endpoint is `/posts/{id}`. Use `putData` for this method. The function should receive id and body as argument. Id should be number between 1 and 100.
+
+```js
+// Sample input
+30,
+{
+    "userId": 1,
+    "title": "Post title",
+    "body": "Post body"
+}
+
+// Sample output
+{
+    "id": 30,
+
+}
+```
+
+- Send DELETE request and delete post, API endpoint is `/posts/{id}`. Use `deleteData` for this method. The function should receive id as argument. Id should be number between 1 and 100.
+
+```js
+// Sample input
+1
+
+// Sample output
+{}
 ```
--- course-lvl-up-fe-1/module-06-web-development-advanced/http/solution.js
+++ course-lvl-up-fe-1/module-06-web-development-advanced/http/solution.js
@@ -1,18 +1,82 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com';
+const apiUrl = "https://jsonplaceholder.typicode.com";
 
-const getPostData = (id) => {
-    if(typeof id !== 'number'){
-        throw Error('Invalid input')
-    }
-    else if(id <= 0 || id > 100){
-        throw Error('Invalid input')
-    }
-    const result = fetch(`${apiUrl}/posts/${id}`)
-    .then(response => response.json())
-    .then(data => data);
-    return result;
+async function getData(id) {
+  if (typeof id !== "number") {
+    throw Error("Invalid input");
+  } else if (id <= 0 || id > 100) {
+    throw Error("Invalid input");
+  }
+
+  const result = fetch(`${apiUrl}/posts/${id}`)
+    .then((response) => response.json())
+    .then((data) => data);
+
+  return result;
 }
 
-module.exports = {
-    getPostData
-};
+async function postData(body) {
+  if (typeof body !== "object") {
+    throw Error("Invalid input");
+  } else if ([body.userId, body.title, body.body].includes(undefined)) {
+    throw Error("Invalid input");
+  }
+  const result = fetch(`${apiUrl}/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application-json",
+    },
+    body,
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+  return result;
+}
+
+async function putData(id, body) {
+  if (typeof body !== "object") {
+    throw Error("Invalid input");
+  } else if ([body.userId, body.title, body.body].includes(undefined)) {
+    throw Error("Invalid input");
+  }
+
+  if (typeof id !== "number") {
+    throw Error("Invalid input");
+  } else if (id <= 0 || id > 100) {
+    throw Error("Invalid input");
+  }
+
+  const result = fetch(`${apiUrl}/posts/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application-json",
+    },
+    body,
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+  return result;
+}
+
+async function deleteData(id) {
+  if (typeof id !== "number") {
+    throw Error("Invalid input");
+  } else if (id <= 0 || id > 100) {
+    throw Error("Invalid input");
+  }
+
+  const result = fetch(`${apiUrl}/posts/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+
+  return result;
+}
+
+/*********************************************
+ * DO NOT MODIFY THIS AREA
+ *
+ * This area is used by the automated tests.
+ ********************************************/
+
+module.exports = { getData, postData, putData, deleteData };
