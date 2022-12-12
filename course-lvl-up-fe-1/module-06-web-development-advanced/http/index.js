1a4195edda33d89aa10ab8dae7a44ba8a7e681fb
Zhivko Uzunov <zhivko.uzunov@mkit.io>
2022-12-12 09:55:37 +0200

    fix: export functions

--- course-lvl-up-fe-1/module-06-web-development-advanced/http/index.js
+++ course-lvl-up-fe-1/module-06-web-development-advanced/http/index.js
@@ -22,11 +22,17 @@ function login() {}
 
 function authorizedRequest() {}
 
-
 /*********************************************
  * DO NOT MODIFY THIS AREA
  *
  * This area is used by the automated tests.
  ********************************************/
 
-module.exports = { getData, postData, putData, deleteData };
+module.exports = {
+  getData,
+  postData,
+  putData,
+  deleteData,
+  login,
+  authorizedRequest,
+};
