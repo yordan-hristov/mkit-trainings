803e03b9ec08dd906f297b1c9b06a88dd2289076
Zhivko Uzunov <zhivko.uzunov@mkit.io>
2022-12-14 15:24:15 +0200

    feature: add second exercise

--- course-lvl-up-fe-1/module-05-web-development-essentials/sass/README.md
+++ course-lvl-up-fe-1/module-05-web-development-essentials/sass/README.md
@@ -8,13 +8,14 @@ CSS is powerful but it still lacks some features and syntax sugar that can help 
 
 ## Exercise #1 - Transpile File
 
-In this Exercise, you are provided with `transpileFile` function declarations and `exercise_1.sass` file.
+In this Exercise, you are provided with `transpileFile` function declarations and `exercise_1.scss` file.
 
 Your objectives are to:
 
 - Implement function that does not receives arguments.
 - Implement function as asynchronous.
 - Transpile the `exercise_1.sass` to `css` file.
+- Use relative path to the file.
 - Use `sass` to transpile the file.
 - Not use `compile()` function of `sass` object.
 - Return the string from the css file.
@@ -24,3 +25,33 @@ Example:
 ```js
 transpileFile().then(res => console.log(res))// h1 {\n  color: #ffffff;\n}\nh1 .test {\n  color: #000000;\n}
 ```
+
+## Exercise #2 - Apply SCSS Styles
+
+In this Exercise, you are provided with `exercise_2.scss`.
+
+Your objectives are to:
+- Create class with name `wrapper` that:
+  - Has rounded borders (`10px`)
+  - Each text in paragraph in the class has red color
+  - Each container background to be in blue
+- Create class with name `item` that:
+  - Has text in `cyan` color
+  - Has white background
+  - If the item with this class has id `special`, text color need to be in `green` color
+- Format file before running tests
+- 
+## Exercise #2 - Apply SCSS Styles
+
+In this Exercise, you are provided with `exercise_2.scss`.
+
+Your objectives are to:
+- Create class with name `wrapper` that:
+  - Has rounded borders (`10px`)
+  - Each text in paragraph in the class has red color
+  - Each container background to be in blue
+- Create class with name `item` that:
+  - Has text in `cyan` color
+  - Has white background
+  - If the item with this class has id `special`, text color need to be in `green` color
+- Format file before running tests
--- course-lvl-up-fe-1/module-05-web-development-essentials/sass/index.test.js
+++ course-lvl-up-fe-1/module-05-web-development-essentials/sass/index.test.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const sass = require('sass');
+
 const { transpileFile } = require("./solution");
 
 describe("Exercise 1 - Transpile File", () => {
@@ -9,6 +12,82 @@ describe("Exercise 1 - Transpile File", () => {
     it("Should return result of the correct type", async () => {
         const expectedResult = 'h1 {\n  color: #ffffff;\n}\nh1 .test {\n  color: #000000;\n}'
         await expect(transpileFile()).resolves.toBe(expectedResult);
+    })
+  });
+});
+
+describe("Exercise 2 - Apply Styles", () => {
+  describe("transpileFile()", () => {
+    let transpiledFile = '';
+    beforeEach(async () => {
+      try{
+        const resultFile = await sass.compileAsync('course-lvl-up-fe-1/module-05-web-development-essentials/sass/exercise_2.scss');
+        transpiledFile =  resultFile.css
+      }
+      catch(error){
+        transpiledFile = error
+      }
+    });
+
+    it("Should exercise_2.scss exists", () => {
+      expect(transpiledFile.message).not.toBeTruthy()
+      expect(typeof transpiledFile).not.toBe('object');
+      expect(typeof transpiledFile).not.toBe('undefined');
+      expect(typeof transpiledFile).not.toBe('boolean');
+      expect(typeof transpiledFile).not.toBe('number');
+      expect(typeof transpiledFile).not.toBe('null');
+      expect(typeof transpiledFile).toBe('string');
+    })
+
+    it("Should use scss syntax", () => {
+      const file = fs.readFileSync('course-lvl-up-fe-1/module-05-web-development-essentials/sass/exercise_2.scss');
+      const isTheSameFile = file.toString().replace(/\s/g, '') === transpiledFile.replace(/\s/g, '');
+      expect(isTheSameFile).toBeFalsy();
+    })
+
+    it("Should have styles for class wrapper", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.wrapper');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have set border radius of class wrapper to 10px", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.wrapper{border-radius:10px;}');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have styles for paragraph with class wrapper", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.wrapperp');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have set color of text in paragraph with class wrapper to red", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.wrapperp{color:red;}');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have set background color of container with class wrapper to blue", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.wrapperdiv{background-color:blue;}');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have styles for class item", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.item');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+    
+    it("Should have set color of class item to cyan", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('color:cyan;');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have set background-color of class item to white", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('background-color:white;');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
+    })
+
+    it("Should have set color to element with class item and id special to green", () => {
+      const checkIfStringExists = transpiledFile.replace(/\s/g, '').lastIndexOf('.item#special{color:green;}');
+      expect(checkIfStringExists).toBeGreaterThanOrEqual(0)
     })
   });
 });
