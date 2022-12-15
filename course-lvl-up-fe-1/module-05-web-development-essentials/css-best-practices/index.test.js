e9111d4a84bc405e369db8afa70981db3867469a
Martin Bonchev <martin.bonchev@mkit.io>
2022-12-15 14:33:37 +0200

    feat: css

--- course-lvl-up-fe-1/module-05-web-development-essentials/css-best-practices/index.test.js
+++ course-lvl-up-fe-1/module-05-web-development-essentials/css-best-practices/index.test.js
@@ -16,6 +16,7 @@ describe("Module - Css Best Practices", () => {
 
     const head = document.querySelector("head");
     const styles = document.createElement("style");
+    styles.className = "test-styles";
 
     styles.innerHTML = cssFileToString;
 
@@ -278,6 +279,371 @@ describe("Module - Css Best Practices", () => {
         const { borderRadius } = buttonStyles;
 
         expect(borderRadius).toBe("12px");
+      });
+    });
+  });
+
+  describe("Exercise 3 - Select Nested Elements", () => {
+    const todoListHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="./solution.css" />
+        <title>Workellence</title>
+      </head>
+      <body>
+        <main>
+          <section>
+            <h1>How to become a front end web developer</h1>
+            <div>
+              <ul id="todo-list">
+                <li>
+                  Learn HTML:
+                  <ul>
+                    <li>Learn tags</li>
+                    <li>
+                      Learn HTML structure:
+                      <ul>
+                        <li class="test-1">Learn HTML parsing</li>
+                        <li class="test-1">Learn accessability</li>
+                      </ul>
+                    </li>
+                    <li>Learn attributes</li>
+                    <li>Learn SEO</li>
+                  </ul>
+                </li>
+                <li>
+                  Learn CSS:
+                  <ul>
+                    <li>Learn selectors</li>
+                    <li id="test-2">Learn specificity</li>
+                    <li>Learn pseudo classes</li>
+                    <li>Learn BEM convention</li>
+                  </ul>
+                </li>
+                <li>
+                  Learn Javascript:
+                  <ul>
+                    <li>
+                      Basics:
+                      <ul>
+                        <li>Learn primitive and referential types</li>
+                        <li id="test-3">Learn operators</li>
+                        <li>Learn if statements</li>
+                        <li>Learn for loops</li>
+                      </ul>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      Fundamentals:
+                      <ul>
+                        <li>Learn object processing</li>
+                        <li>Learn array processing</li>
+                        <li>Learn string processing</li>
+                        <li id="test-4">Learn regex</li>
+                      </ul>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      Advance:
+                      <ul>
+                        <li class="test-5">Learn HTTP</li>
+                        <li class="test-5">Learn Modules and Module Bundlers</li>
+                        <li class="test-5">Learn Compilers/Interpretators/Transpilers</li>
+                        <li class="test-5">Learn DOM</li>
+                        <li class="test-5">Learn Testing</li>
+                      </ul>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      Proficient:
+                      <ul>
+                        <li class="test-6">Learn React</li>
+                        <li class="test-6">Learn Virtual DOM</li>
+                        <li class="test-6">Learn React Hooks</li>
+                        <li class="test-6">Learn Redux</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </main>
+      </body>
+    </html>`;
+
+    beforeEach(() => {
+      document.documentElement.innerHTML = todoListHTML;
+
+      const head = document.querySelector("head");
+      const styles = document.createElement("style");
+      styles.className = "test-styles";
+
+      styles.innerHTML = cssFileToString;
+
+      head.appendChild(styles);
+    });
+
+    afterAll(() => {
+      document.documentElement.innerHTML = fileToString;
+
+      const head = document.querySelector("head");
+      const styles = document.createElement("style");
+      styles.className = "test-styles";
+
+      styles.innerHTML = cssFileToString;
+
+      head.appendChild(styles);
+    });
+
+    it(`Should have "Learn HTML structure" list items red`, () => {
+      const listItems = document.querySelectorAll(".test-1");
+
+      const listItemsColors = Array.from(listItems).map((listItem) => {
+        const listItemStyles = getComputedStyle(listItem);
+
+        const { color } = listItemStyles;
+
+        return color;
+      });
+
+      const isEveryListItemRed = listItemsColors.every(
+        (color) => color === "red"
+      );
+
+      expect(isEveryListItemRed).toBeTruthy();
+    });
+
+    it(`Should have "Learn specificity"`, () => {
+      const listItem = document.querySelector("#test-2");
+
+      const listItemStyles = getComputedStyle(listItem);
+      const { color } = listItemStyles;
+
+      expect(color).toBe("yellow");
+    });
+
+    it(`Should have "Learn operators"`, () => {
+      const listItem = document.querySelector("#test-3");
+
+      const listItemStyles = getComputedStyle(listItem);
+      const { color } = listItemStyles;
+
+      expect(color).toBe("blue");
+    });
+
+    it(`Should have "Learn regex bullet"`, () => {
+      const listItem = document.querySelector("#test-4");
+
+      const listItemStyles = getComputedStyle(listItem);
+      const { color } = listItemStyles;
+
+      expect(color).toBe("green");
+    });
+
+    it(`Should have "Advance" list items violet`, () => {
+      const listItems = document.querySelectorAll(".test-5");
+
+      const listItemsColors = Array.from(listItems).map((listItem) => {
+        const listItemStyles = getComputedStyle(listItem);
+
+        const { color } = listItemStyles;
+
+        return color;
+      });
+
+      const isEveryListItemRed = listItemsColors.every(
+        (color) => color === "violet"
+      );
+
+      expect(isEveryListItemRed).toBeTruthy();
+    });
+
+    it(`Should have "Proficiency" list items violet`, () => {
+      const listItems = document.querySelectorAll(".test-6");
+
+      const listItemsColors = Array.from(listItems).map((listItem) => {
+        const listItemStyles = getComputedStyle(listItem);
+
+        const { color } = listItemStyles;
+
+        return color;
+      });
+
+      const isEveryListItemRed = listItemsColors.every(
+        (color) => color === "cyan"
+      );
+
+      expect(isEveryListItemRed).toBeTruthy();
+    });
+  });
+
+  describe("Exercise 4 - Use Of BEM", () => {
+    const bemStyles = `
+    .footer {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      height: 50px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-top: solid 1px #c5c5c5;
+      background-color: white;
+    }
+    
+    .footer__copyright {
+      margin-left: 24px;
+      font-weight: 600;
+      font-size: 14px;
+    }
+    
+    .footer__social-media {
+      display: flex;
+      flex-direction: row;
+      gap: 32px;
+      margin-right: 24px;
+    }
+    
+    .social-media__social-item--platform {
+      color: #777789;
+    }
+    `;
+
+    beforeEach(() => {
+      const head = document.querySelector("head");
+      const testStyles = document.querySelector(".test-styles");
+      const bemStylesElement = document.createElement("style");
+      head.removeChild(testStyles);
+      bemStylesElement.className = "bem-styles";
+      bemStylesElement.innerHTML = bemStyles;
+
+      head.appendChild(bemStylesElement);
+    });
+
+    describe("footer", () => {
+      let footerStyles = null;
+
+      beforeEach(() => {
+        const footer = document.querySelector("footer");
+        footerStyles = getComputedStyle(footer);
+      });
+
+      it("Should have position equals fixed", () => {
+        const { position } = footerStyles;
+
+        expect(position).toEqual("fixed");
+      });
+
+      it("Should be with bottom equals 0", () => {
+        const { bottom } = footerStyles;
+
+        expect(bottom).toEqual("0px");
+      });
+
+      it("Should have width equals 100%", () => {
+        const { width } = footerStyles;
+
+        expect(width).toEqual("100%");
+      });
+
+      it("Should have height equals 50px", () => {
+        const { height } = footerStyles;
+
+        expect(height).toEqual("50px");
+      });
+
+      it("Should have display equals flex", () => {
+        const { display } = footerStyles;
+
+        expect(display).toEqual("flex");
+      });
+
+      it("Should have justify content equals space between", () => {
+        const { justifyContent } = footerStyles;
+
+        expect(justifyContent).toEqual("space-between");
+      });
+
+      it("Should have align items equals flex", () => {
+        const { alignItems } = footerStyles;
+
+        expect(alignItems).toEqual("center");
+      });
+
+      it("Should have border top equals 1px solid #c5c5c5", () => {
+        const { borderTop } = footerStyles;
+
+        expect(borderTop).toEqual("1px solid #c5c5c5");
+      });
+
+      it("Should have background color equals white", () => {
+        const { backgroundColor } = footerStyles;
+
+        expect(backgroundColor).toEqual("white");
+      });
+    });
+
+    describe("copyright", () => {
+      let copyrightStyles = null;
+      beforeEach(() => {
+        const copyright = document.querySelector(".footer__copyright");
+        copyrightStyles = getComputedStyle(copyright);
+      });
+      it("Should have margin left equals 24px", () => {
+        const { marginLeft } = copyrightStyles;
+        expect(marginLeft).toEqual("24px");
+      });
+      it("Should have font weight equals 600", () => {
+        const { fontWeight } = copyrightStyles;
+        expect(Number(fontWeight)).toEqual(600);
+      });
+      it("Should have font size equals 14px", () => {
+        const { fontSize } = copyrightStyles;
+        expect(fontSize).toEqual("14px");
+      });
+    });
+    describe("social-media ", () => {
+      let socialMediaStyles = null;
+      beforeEach(() => {
+        const socialMedia = document.querySelector(".footer__social-media");
+        socialMediaStyles = getComputedStyle(socialMedia);
+      });
+      it("Should have display equals flex", () => {
+        const { display } = socialMediaStyles;
+        expect(display).toEqual("flex");
+      });
+      it("Should have flex direction equals row", () => {
+        const { flexDirection } = socialMediaStyles;
+        expect(flexDirection).toEqual("row");
+      });
+      it("Should have gap equals 32px", () => {
+        const { gap } = socialMediaStyles;
+        expect(gap).toEqual("32px");
+      });
+      it("Should have margin right equals 24px", () => {
+        const { marginRight } = socialMediaStyles;
+        expect(marginRight).toEqual("24px");
+      });
+    });
+    describe("platform", () => {
+      let platformStyles = null;
+      beforeEach(() => {
+        const platform = document.querySelector(
+          ".social-media__social-item--platform"
+        );
+        platformStyles = getComputedStyle(platform);
+      });
+      it("Should have color equals rgb(119, 119, 137)", () => {
+        const { color } = platformStyles;
+        expect(color).toEqual("rgb(119, 119, 137)");
       });
     });
   });
